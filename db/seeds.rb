120a0b47d2024cf5d656172eca0fb78e66b4faa7
sudarshan gupta <sudarshan395@gmail.com>
2021-04-02 15:28:47 +0530

    Add message partial and refactor code

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,10 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first)\
+
+User.create(username: "sudarshan1", password: "password1")
+User.create(username: "sudarshan2", password: "password2")
+User.create(username: "sudarshan3", password: "password3")
+User.create(username: "sudarshan4", password: "password4")
+User.create(username: "sudarshan5", password: "password5")

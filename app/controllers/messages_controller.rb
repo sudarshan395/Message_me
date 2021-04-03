e448246d8b6cf07a13f9e41e797ff2e8c48d98bc
sudarshan gupta <sudarshan395@gmail.com>
2021-04-03 12:36:46 +0530

    Add auto scrolling to chat window

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,22 @@
 class MessagesController < ApplicationController
+  before_action :require_user
 
-	before_action :require_user
+  def create
+    message = current_user.messages.build(message_params)
+    if message.save
+      ActionCable.server.broadcast "chatroom_channel",
+                                    mod_message: message_render(message)
+    end
+  end
 
-	def create
-		message = current_user.messages.build(message_params)
-		if message.save
-			redirect_to root_path
-		end
-	end
+  private
 
-	private
+  def message_params
+    params.require(:message).permit(:body)
+  end
 
-	def message_params
-		params.require(:message).permit(:body)
-	end
-
+  def message_render(message)
+    render(partial: 'message', locals: {message: message})
+  end
 
 end
